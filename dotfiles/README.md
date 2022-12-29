06030e83c9513977de414cec6a5b17f125743c9d
9glenda <plan9git@proton.me>
2022-12-29 23:12:06 +0000

    stuff

---
+++ dotfiles/README.md
@@ -0,0 +1,7 @@
+# Dotfiles
+
+to apply the nix config simply run:
+```sh
+nix-shell
+colmena build;colmena apply-local --sudo --node main
+```
--- dotfiles/configs/main/default.nix
+++ dotfiles/configs/main/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ./hardware-configuration.nix
@@ -14,4 +14,22 @@
   boot.zfs.requestEncryptionCredentials = true;
   system.stateVersion = "23.05"; # Did you read the comment?
   networking.hostName = "main";
+  environment.gnome.excludePackages = (with pkgs; [
+    gnome-photos
+    gnome-tour
+  ]) ++ (with pkgs.gnome; [
+    cheese # webcam tool
+    gnome-music
+    gnome-terminal
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    evince # document viewer
+    gnome-characters
+    totem # video player
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+  ]);
 }
