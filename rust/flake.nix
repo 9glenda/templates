4e62c62aa2774c28ae5685051604b8dae2fdb37d
9glenda <plan9git@proton.me>
2023-10-06 00:19:27 +0200

    rust flake

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks-nix.url = "github:cachix/pre-commit-hooks.nix";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     treefmt-nix.url = "github:numtide/treefmt-nix";
     rust-overlay.url = "github:oxalica/rust-overlay";
@@ -14,63 +15,62 @@
     };
   };
 
-  outputs = inputs @ {
-    self,
-    crane,
-    nixpkgs,
-    treefmt-nix,
-    rust-overlay,
-    flake-parts,
-    ...
-  }:
-    flake-parts.lib.mkFlake {inherit inputs;} {
-      # imports = [
-      #   inputs.treefmt-nix.flakeModule
-      # ];
+  outputs =
+    inputs @ { self
+    , crane
+    , nixpkgs
+    , treefmt-nix
+    , rust-overlay
+    , flake-parts
+    , ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed;
-      perSystem = {
-        pkgs,
-        system,
-        ...
-      }: let
-        cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-        overlays = [(import rust-overlay)];
-        pkgs = (import nixpkgs) {
-          inherit system overlays;
-        };
-        inherit (pkgs) lib;
-        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      perSystem =
+        { pkgs
+        , system
+        , ...
+        }:
+        let
+          cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+          overlays = [ (import rust-overlay) ];
+          pkgs = (import nixpkgs) {
+            inherit system overlays;
+          };
+          inherit (pkgs) lib;
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+          treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
 
-        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+          craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
 
-        src = craneLib.cleanCargoSource (craneLib.path ./.);
+          src = craneLib.cleanCargoSource (craneLib.path ./.);
 
-        commonArgs = {
-          inherit src;
+          commonArgs = {
+            inherit src;
 
-          buildInputs = with pkgs; [
-            zlib
-            git
+            buildInputs = with pkgs; [
+              zlib
+              git
+            ];
+          };
+
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          cargoPlugins = with pkgs; [
+            cargo-expand
+            cargo-modules
+            cargo-nextest
+            cargo-watch
+            # cargo-criterion
+            # cargo-llvm-cov
+            bacon
           ];
-        };
-
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        cargoPlugins = with pkgs; [
-          cargo-expand
-          cargo-modules
-          cargo-nextest
-          cargo-watch
-          # cargo-criterion
-          # cargo-llvm-cov
-          bacon
-        ];
-      in rec {
-        formatter = treefmtEval.config.build.wrapper;
-        packages = {
-          default = craneLib.buildPackage (commonArgs
-            // rec {
+        in
+        rec {
+          formatter = treefmtEval.config.build.wrapper;
+          packages = {
+            default = craneLib.buildPackage (commonArgs
+              // rec {
               inherit cargoArtifacts;
               pname = "${cargo.package.name}";
               version = "${cargo.package.version}";
@@ -81,24 +81,25 @@
                 homepage = "${cargo.package.repository}";
                 license = licenses.gpl3;
                 mainProgram = "${cargo.package.name}";
-                maintainers = with maintainers; [_9glenda];
+                maintainers = with maintainers; [ _9glenda ];
               };
             });
-        };
-        # For `nix develop`:
-        devShells = {
-          default = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [toolchain cargoPlugins];
+            rust-flake-nextest = craneLib.cargoNextest (commonArgs
+              // {
+              inherit cargoArtifacts;
+              cargoNextestExtraArgs = "--color=never";
+            });
+          };
+          # For `nix develop`:
+          devShells = {
+            default = pkgs.mkShell {
+              nativeBuildInputs = with pkgs; [ toolchain cargoPlugins ];
+            };
+          };
+          checks = {
+            formatting = treefmtEval.config.build.check self;
+            inherit (packages) rust-flake-nextest;
           };
         };
-        checks = {
-          formatting = treefmtEval.config.build.check self;
-        };
-      };
     };
-  # flake-utils.lib.eachDefaultSystem (
-  #   system: let
-  #   in rec {
-  #   }
-  # );
 }
